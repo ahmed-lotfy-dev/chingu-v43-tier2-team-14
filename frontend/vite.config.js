69014845a020c0695f83084e71a8ea46ac5871b7
ahmed-lotfy-dev <elshenawy19@gmail.com>
2024-10-19 17:03:41 +0300

    editing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,11 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "/api": {
-        target: "http://localhost:4000/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
+      api: "http://localhost:4000",
     },
   },
   plugins: [
