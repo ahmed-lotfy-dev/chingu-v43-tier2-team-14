fb8c1e8550dced735412d5639ddfba8492f13d71
ahmed-lotfy-dev <elshenawy19@gmail.com>
2025-05-14 20:39:51 +0300

    Fix social login better-auth working

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,16 @@ import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   server: {
     port: 3000,
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:4000",
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ""),
-  //     },
-  //   },
+    //   proxy: {
+    //     "/api": {
+    //       target: "http://localhost:4000",
+    //       changeOrigin: true,
+    //       rewrite: (path) => path.replace(/^\/api/, ""),
+    //     },
+    //   },
   },
   plugins: [react()],
 })
