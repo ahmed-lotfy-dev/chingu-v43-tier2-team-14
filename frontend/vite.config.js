75c13e8382fbd893751f00c05388c37820472e5d
ahmed-lotfy-dev <elshenawy19@gmail.com>
2025-05-14 18:01:33 +0300

    fix some imports after changing type place

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,13 +7,13 @@ dotenv.config()
 export default defineConfig({
   server: {
     port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:4000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "http://localhost:4000",
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ""),
+    //   },
+    // },
   },
   plugins: [
     react({
