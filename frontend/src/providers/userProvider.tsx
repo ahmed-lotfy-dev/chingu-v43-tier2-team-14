5389baaa59d02a17d5979f0696bbf9585522ba29
ahmed-lotfy-dev <elshenawy19@gmail.com>
2025-05-22 11:33:58 +0300

    fix all linting errors for fix deployment

--- frontend/src/providers/userProvider.tsx
+++ frontend/src/providers/userProvider.tsx
@@ -23,7 +23,7 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
 
   const logout = async () => {
     setUserState(null)
-    await authClient.logout()
+    await authClient.signOut()
     localStorage.removeItem("user")
   }
 
